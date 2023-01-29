a3880035fcc3d57b006148efdfedd23c033741e0
raon9401 <raon9401@naver.com>
2023-01-29 20:13:16 +0900

    1월 4주차 학습

---
+++ 정희찬/기본 개념 실습/loading.ts
@@ -0,0 +1,42 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: "loading";
+  };
+
+  type SuccessState = {
+    state: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: "fail";
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  const printLoginState = (state: ResourceLoadState) => {
+    switch (state.state) {
+      case "loading":
+        console.log("loading...");
+        break;
+      case "success":
+        console.log(`${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  };
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+}
